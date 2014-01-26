afb60183daee0d60652522769fb7500efb07cad1
dieselpoweredkitten <me@require.pm>
2014-01-26 12:07:55 +0400

    Prepare for PyPI release

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+
 
 setup(
   name = 'termformat',
+  url = 'https://github.com/dieselpoweredkitten/termformat',
+  author = 'termformat contributors',
+  author_email = 'github@require.pm',
   version = '0.1',
   description = 'Erlang External Term Format (de)serialization module',
   license = 'MIT',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = [Extension('termformat', ['termformat.pyx'])],
+  ext_modules = [
+    Extension('termformat', ['termformat.c'])
+  ],
   classifiers = (
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
-  )
+  ),
 )
