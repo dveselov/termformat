c4505c07668d79d8180ccb71337b06d03a3467db
dieselpoweredkitten <me@require.pm>
2014-03-06 21:54:05 +0400

    more idiomatic code

--- termformat.pyx
+++ termformat.pyx
@@ -1,4 +1,5 @@
-from struct import pack, unpack
+# coding: utf-8
+import struct
 
 __version__ = "0.1.1"
 
@@ -14,21 +15,21 @@ cdef bytes ERL_NEW_FLOAT, ERL_COMPRESSED, ERL_SMALL_INT, ERL_INT, ERL_FLOAT, ERL
 
 DEFAULT_ENCODING = "utf-8"
 
-ERL_NEW_FLOAT = pack('B', 70)
-ERL_COMPRESSED = pack('B', 80)
-ERL_SMALL_INT = pack('B', 97)
-ERL_INT = pack('B', 98)
-ERL_FLOAT = pack('B', 99)
-ERL_ATOM = pack('B', 100)
-ERL_SMALL_TUPLE = pack('B', 104)
-ERL_LARGE_TUPLE = pack('B', 105)
-ERL_NIL = pack('B', 106)
-ERL_STRING = pack('B', 107)
-ERL_LIST = pack('B', 108)
-ERL_BINARY = pack('B', 109)
-ERL_SMALL_BIGNUM = pack('B', 110)
-ERL_LARGE_BIGNUM = pack('B', 111)
-ERL_MAGIC = pack('B', 131)
+ERL_NEW_FLOAT = b'F'
+ERL_COMPRESSED = b'P'
+ERL_SMALL_INT = b'a'
+ERL_INT = b'b'
+ERL_FLOAT = b'c'
+ERL_ATOM = b'd'
+ERL_SMALL_TUPLE = b'h'
+ERL_LARGE_TUPLE = b'i'
+ERL_NIL = b'j'
+ERL_STRING = b'k'
+ERL_LIST = b'l'
+ERL_BINARY = b'm'
+ERL_SMALL_BIGNUM = b'n'
+ERL_LARGE_BIGNUM = b'o'
+ERL_MAGIC = b'\x83'
 
 
 cdef bytes encode_term(object term):
@@ -42,21 +43,21 @@ cdef bytes encode_term(object term):
     return encode_term(":undefined")
   elif term_type in (int, long):
     if 0 <= term <= 255:
-      return ERL_SMALL_INT + pack('B', term)
+      return ERL_SMALL_INT + struct.pack('B', term)
     elif -2147483648 <= term <= 2147483647:
-      return ERL_INT + pack('>l', term)
+      return ERL_INT + struct.pack('>l', term)
     else:
       sign, term = (0, term) if term >= 0 else (1, -term)
       body = b""
       while term:
-        body += pack('B', term & 0xff)
+        body += struct.pack('B', term & 0xff)
         term >>= 8
       length = len(body)
-      sign = pack('B', sign)
+      sign = struct.pack('B', sign)
       if length <= 255:
-        return ERL_SMALL_BIGNUM + pack('B', length) + sign + body
+        return ERL_SMALL_BIGNUM + struct.pack('B', length) + sign + body
       elif length <= 4294967295:
-        return ERL_LARGE_BIGNUM + pack('>L', length) + sign + body
+        return ERL_LARGE_BIGNUM + struct.pack('>L', length) + sign + body
       else:
         raise ValueError("Invalid BIGNUM_EXT length: {0}".format(length))
   elif term_type is float:
@@ -71,11 +72,11 @@ cdef bytes encode_term(object term):
       if not length or length > 255:
         raise ValueError("Invalid ATOM_EXT length: {0}".format(term))
       else:
-        return ERL_ATOM + pack('>H', length) + atom_name
+        return ERL_ATOM + struct.pack('>H', length) + atom_name
     else:
       length = len(term)
       if length <= 4294967295:
-        return ERL_BINARY + pack('>L', length) + term
+        return ERL_BINARY + struct.pack('>L', length) + term
       else:
         raise ValueError("Invalid BINARY_EXT length: {0}".format(length))
   elif term_type in (str, unicode):
@@ -84,9 +85,9 @@ cdef bytes encode_term(object term):
   elif term_type is tuple:
     length = len(term)
     if length <= 255:
-      tuple_type, tuple_length = ERL_SMALL_TUPLE, pack('B', length)
+      tuple_type, tuple_length = ERL_SMALL_TUPLE, struct.pack('B', length)
     elif length <= 4294967295:
-      tuple_type, tuple_length = ERL_LARGE_TUPLE, pack('>L', length)
+      tuple_type, tuple_length = ERL_LARGE_TUPLE, struct.pack('>L', length)
     else:
       raise ValueError("Invalid TUPLE_EXT length: {0}".format(length))
     content = tuple_type + tuple_length
@@ -98,7 +99,7 @@ cdef bytes encode_term(object term):
     if not length:
       return ERL_NIL
     elif length <= 4294967295:
-      body = pack('>L', length)
+      body = struct.pack('>L', length)
       for item in term:
         body += encode_term(item)
       return ERL_LIST + body + ERL_NIL
@@ -121,19 +122,19 @@ cdef object decode_term(bytes term):
     if not body:
       raise ValueError("Incomplete SMALL_INT_EXT length: expected 1, got 0")
     else:
-      return unpack('B', body)[0], term[2:]
+      return struct.unpack('B', body)[0], term[2:]
   elif term_type == ERL_INT:
     body = term[1:5]
     if len(body) < 4:
       raise ValueError("Incomplete INT_EXT length: expected 4, got {0}".format(len(body)))
     else:
-      return unpack('>l', body)[0], term[5:]
+      return struct.unpack('>l', body)[0], term[5:]
   elif term_type in (ERL_SMALL_BIGNUM, ERL_LARGE_BIGNUM):
     if term_type == ERL_SMALL_BIGNUM:
-      length, sign = unpack('B', term[1:2])[0], ord(term[2:3])
+      length, sign = struct.unpack('B', term[1:2])[0], ord(term[2:3])
       tail = term[3:]
     else:
-      length, sign = unpack('>L', term[1:5])[0], ord(term[5:6])
+      length, sign = struct.unpack('>L', term[1:5])[0], ord(term[5:6])
       tail = term[4:]
     n = 0
     if length:
@@ -156,9 +157,9 @@ cdef object decode_term(bytes term):
     if len(body) < 8:
       raise ValueError("Incomplete NEW_FLOAT_EXT length: expected 8, got {0}".format(len(body)))
     else:
-      return unpack('>d', body)[0], term[9:]
+      return struct.unpack('>d', body)[0], term[9:]
   elif term_type == ERL_STRING:
-    length = unpack(">H", term[1:3])[0]
+    length = struct.unpack(">H", term[1:3])[0]
     body = term[3:length + 3]
     if length > 65535:
       raise ValueError("Invalid STRING_EXT length: {0}".format(length))
@@ -167,7 +168,7 @@ cdef object decode_term(bytes term):
     else:
       return body.decode(DEFAULT_ENCODING), term[length:]
   elif term_type == ERL_BINARY:
-    length = unpack('>L', term[1:5])[0] + 5
+    length = struct.unpack('>L', term[1:5])[0] + 5
     body = term[5:length]
     if length > 4294967295:
       raise ValueError("Invalid BINARY_EXT length: {0}".format(length))
@@ -176,28 +177,28 @@ cdef object decode_term(bytes term):
     else:
       return body.decode(DEFAULT_ENCODING), term[length:]
   elif term_type == ERL_SMALL_TUPLE:
-    length, = unpack('B', term[1:2])
+    length, = struct.unpack('B', term[1:2])
     if length > 255:
       raise ValueError("Invalid SMALL_TUPLE_EXT length: {0}".format(length))
     else:
       objects, tail = decode_iterable(length, term[2:])
       return tuple(objects), tail
   elif term_type == ERL_LARGE_TUPLE:
-    length, = unpack('>L', term[1:5])
+    length, = struct.unpack('>L', term[1:5])
     if length > 4294967295:
       raise ValueError("Invalid LARGE_TUPLE_EXT length: {0}".format(length))
     else:
       objects, tail = decode_iterable(length, term[5:])
       return tuple(objects), tail
   elif term_type == ERL_LIST:
-    length, = unpack('>L', term[1:5])
+    length, = struct.unpack('>L', term[1:5])
     if length > 4294967295:
       raise ValueError("Invalid LIST_EXT length: {0}".format(length))
     else:
       objects, tail = decode_iterable(length, term[5:])
       return objects, tail
   elif term_type == ERL_ATOM:
-    atom_length = unpack('>H', term[1:3])[0] + 3
+    atom_length = struct.unpack('>H', term[1:3])[0] + 3
     atom_name = term[3:atom_length]
     if atom_length > len(atom_name) + 3:
       raise ValueError("Invalid ATOM_EXT length: expected {0}, got {1}".format(atom_length, len(atom_name)))
@@ -224,9 +225,9 @@ cdef tuple decode_iterable(int length, bytes source):
     if source:
       term, source = decode_term(source)
       objects.append(term)
-      length -= 1
     else:
       raise ValueError("Incomplete iterable length")
+    length -= 1
   if source[:1] == ERL_NIL:
     source = source[1:]
   return objects, source
